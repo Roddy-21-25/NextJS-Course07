ae63cf6de98d06a2ba4941a2c7995e2c50a50e0d
Roddy-Rafael-Tejeda-Rosario-21-25 <128174391+Roddy-21-25@users.noreply.github.com>
2024-01-04 23:26:06 -0400

    first commit

---
+++ src/app/api/echo/route.ts
@@ -0,0 +1,6 @@
+import { NextResponse } from "next/server";
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const obj = Object.fromEntries(searchParams.entries());
+  return NextResponse.json(obj);
+}
